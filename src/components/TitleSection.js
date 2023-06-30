7f34d2bb569e7579c4851f18533f221e63f6247c
sharonchen9593 <sharonchen9593@gmail.com>
2023-06-29 21:28:18 -0700

    Fix title section

--- src/components/TitleSection.js
+++ src/components/TitleSection.js
@@ -4,7 +4,7 @@ const TitleSection = () => {
   return (
     <section className="title-section">
       <h1>Sharon Chen</h1>
-      <h3>Software Engineer</h3>
+      <h3>Software Engineer - San Francisco Bay Area</h3>
       <div className="arrow-down">
         <KeyboardArrowDownIcon fontSize="inherit" />
       </div>
